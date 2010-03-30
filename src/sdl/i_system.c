02fc85cb537f0032f1f8572bd2abd6eb4e202520
alam <alam@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2010-03-30 01:08:17 +0000

    SRB2: no diskspace lookup or Termios suppport for Haiku
    git-svn-id: svn://code.srb2.org/SRB2/trunk@4829 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- src/sdl/i_system.c
+++ src/sdl/i_system.c
@@ -91,7 +91,7 @@ void __set_fpscr(long); // in libgcc / kernel's startup.s?
 #ifdef _PSP
 #include <pspiofilemgr.h>
 #else
-#if defined (__unix__) || defined(__APPLE__) || (defined (UNIXCOMMON) && !defined (_arch_dreamcast))
+#if defined (__unix__) || defined(__APPLE__) || (defined (UNIXCOMMON) && !defined (_arch_dreamcast) && !defined(__HAIKU__)
 #if defined (__linux__)
 #include <sys/vfs.h>
 #else
@@ -108,7 +108,7 @@ void __set_fpscr(long); // in libgcc / kernel's startup.s?
 #endif
 #endif
 
-#if defined (__linux__) || (defined (UNIXCOMMON) && !defined (_arch_dreamcast) && !defined (_PSP))
+#if defined (__linux__) || (defined (UNIXCOMMON) && !defined (_arch_dreamcast) && !defined (_PSP) && !defined (__HAIKU__))
 #ifndef NOTERMIOS
 #include <termios.h>
 #include <sys/ioctl.h> // ioctl
