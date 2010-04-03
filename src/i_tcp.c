a279502a5ed917efd15453131c7ac6349ca327e2
mattw_cfi <mattw_cfi@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2010-04-03 19:04:13 +0000

    SRB2: Instead of removing a necessary 'hack' for ingame/dedicated servers outright, why don't we just make it only do its job if you're playing in the first place?
    git-svn-id: svn://code.srb2.org/SRB2/trunk@4894 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- src/i_tcp.c
+++ src/i_tcp.c
@@ -422,14 +422,15 @@ static signed char getfreenode(void)
 // This goes through and cleans up what nodes were supposed to be freed.
 static void cleanupnodes(void)
 {
-#if 0
+	if (!Playing())
+		return;
+
 	signed char j;
 
 	// Why can't I start at zero?
 	for (j = 1; j < MAXNETNODES; j++)
 		if (!nodeingame[j])
 			nodeconnected[j] = false;
-#endif
 }
 #endif
 
