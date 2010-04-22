2be26f5ee229ccd4cd5deb149c763715a563ae77
alam <alam@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2010-04-22 19:04:47 +0000

    SRB2: oops
    git-svn-id: svn://code.srb2.org/SRB2/trunk@5195 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- src/p_tick.c
+++ src/p_tick.c
@@ -266,7 +266,7 @@ static void P_DoAutobalanceTeams(void)
 	INT32 red=0, blue=0;
 	INT32 redarray[MAXPLAYERS], bluearray[MAXPLAYERS];
 
-	NqetPacket.value.l = NetPacket.value.b = 0;
+	NetPacket.value.l = NetPacket.value.b = 0;
 	qmemset(redarray, 0, sizeof(redarray));
 	memset(bluearray, 0, sizeof(bluearray));
 
