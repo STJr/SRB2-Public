6abae6e2ae427b5a603b705c2f892759517ccf95
alam <alam@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2010-05-28 18:06:55 +0000

    SRB2: updated cpdebug.mk
    git-svn-id: svn://code.srb2.org/SRB2/trunk@5366 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- cpdebug.mk
+++ cpdebug.mk
@@ -3,6 +3,6 @@ OBJCOPY="objcopy.exe"
 DGB=$(BIN).debug
 
 all-after:
-	$(OBJCOPY) --only-keep-debug $(BIN) $(DGB)
+	$(OBJCOPY) $(BIN) $(DGB)
 	$(OBJCOPY) --strip-debug $(BIN)
 	$(OBJCOPY) --add-gnu-debuglink=$(DGB) $(BIN)
