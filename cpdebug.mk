dd3a14bfb88bda8691e0aa3d329c3d06db137ad7
alam <alam@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2010-08-17 00:04:42 +0000

    SRB2: use objdump to dump SRB2 binary debug info into a text file
    git-svn-id: svn://code.srb2.org/SRB2/trunk@6029 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- cpdebug.mk
+++ cpdebug.mk
@@ -4,12 +4,15 @@ COMSPEC=$(ComSpec)
 endif
 ifdef COMSPEC
 OBJCOPY=objcopy.exe
+OBJDUMP=objdump.exe
 else
 OBJCOPY=objcopy
+OBJDUMP=objdump
 endif
 DGB=$(BIN).debug
 
 all-after:
+	$(OBJDUMP) -S -l $(BIN) > $(DGB).txt
 	$(OBJCOPY) $(BIN) $(DGB)
 	$(OBJCOPY) --strip-debug $(BIN)
 	$(OBJCOPY) --add-gnu-debuglink=$(DGB) $(BIN)
