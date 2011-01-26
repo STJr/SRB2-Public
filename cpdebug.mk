7f37d3cd62428e9844ba04a5036771195e87cd73
alam <alam@d18046d3-e9af-4449-a702-ac2b6c676ba2>
2011-01-26 23:17:04 +0000

    SRB2: gzip debugging info text file
    git-svn-id: svn://code.srb2.org/SRB2/trunk@6613 d18046d3-e9af-4449-a702-ac2b6c676ba2

--- cpdebug.mk
+++ cpdebug.mk
@@ -10,9 +10,14 @@ OBJCOPY=objcopy
 OBJDUMP=objdump
 endif
 DGB=$(BIN).debug
+OBJDUMP_OPTS?=--wide --source --line-numbers
+GZIP?=gzip
+GZIP_OPTS?=-9 -f
+GZIP_OPT2=$(GZIP_OPTS) --rsyncable
 
 all-after:
-	$(OBJDUMP) -S -l $(BIN) > $(DGB).txt
+	$(OBJDUMP) $(OBJDUMP_OPTS) $(BIN) > $(DGB).txt
+	-$(GZIP) $(GZIP_OPT2) $(BIN)/$(DBGNAME).txt || -$(GZIP) $(GZIP_OPTS) $(BIN)/$(DBGNAME).txt
 	$(OBJCOPY) $(BIN) $(DGB)
 	$(OBJCOPY) --strip-debug $(BIN)
 	$(OBJCOPY) --add-gnu-debuglink=$(DGB) $(BIN)
